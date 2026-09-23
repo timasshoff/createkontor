8d3c60dd6b595d665a245db6e4841955dee25bff
Tim <82809793+timasshoff@users.noreply.github.com>
2026-09-23 21:27:57 +0200

    Fixes

--- src/main/java/com/timder/kontor/game/command/EconomyCommands.java
+++ src/main/java/com/timder/kontor/game/command/EconomyCommands.java
@@ -54,7 +54,7 @@ public class EconomyCommands {
         int days = IntegerArgumentType.getInteger(context, "days");
         long lastHistoryDayBefore = KontorTickHandler.lastHistoryDay(economy);
 
-        economy.advanceTo(days * Economy.DAY_LENGTH);
+        economy.advanceTo(economy.ticksElapsed() + (long) days * Economy.DAY_LENGTH);
         economyData.setDirty();
         KontorTickHandler.settleCompanies(companyData, economy, lastHistoryDayBefore);
 
